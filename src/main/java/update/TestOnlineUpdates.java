de1b9ee7698b19f26fbe5d4a0244c83e9d342032
Karl Nicholas <karl.nicholas@outlook.com>
2018-08-17 11:40:02 -0700

    Updates StatutesService

--- src/main/java/update/TestOnlineUpdates.java
+++ src/main/java/update/TestOnlineUpdates.java
@@ -1,14 +1,18 @@
 package update;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
+import client.StatutesRsService;
 import opca.parser.OpinionScraperInterface;
-import opca.scraper.CACaseScraper;
 import opca.service.CAOnlineUpdates;
 import scraper.TestCACaseScraper;
+import service.StatutesService;
 
 public class TestOnlineUpdates {
 	private EntityManager em;
@@ -21,13 +25,19 @@ public class TestOnlineUpdates {
 	private void run() {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("opjpa");
 		em = emf.createEntityManager();
+		StatutesService statutesService;
+		try {
+			statutesService = new StatutesRsService(new URL("http://localhost:8080/statutesrs/rs/")).getRsService();
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(e);
+		}
 		try {
 //			OpinionScraperInterface caseScraper = new CACaseScraper(true);
 			OpinionScraperInterface caseScraper = new TestCACaseScraper(false);
 			EntityTransaction tx = em.getTransaction();
 			try {
 				tx.begin();
-				new CAOnlineUpdates(em).updateDatabase(caseScraper);
+				new CAOnlineUpdates(em, statutesService).updateDatabase(caseScraper);
 				tx.commit();
 			} catch (Exception ex) {
 				ex.printStackTrace();
